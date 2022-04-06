463514dcde6639aa49be4b75252c5788da41fc45
souki20 <souki0201@icloud.com>
2022-04-06 15:17:06 +0900

    Remove: コメントアウトを削除
    不要な過去のコメントアウトを削除
    最新のコードに書き換えた

--- src/index.js
+++ src/index.js
@@ -2,19 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 
-// class Square extends React.Component {
-//   render() {
-//     return (
-//       <button 
-//         className="square" 
-//         onClick={() => this.props.onClick()}
-//       >
-//         {this.props.value}
-//       </button>
-//     );
-//   }
-// }
-
 function Square(props) {
   return (
     <button className = {`square ${props.isHighLight ? 'highlight' : ''}`} onClick = {props.onClick}>
@@ -55,22 +42,6 @@ class Board extends React.Component {
             )
           })
         }
-      {/* </div>
-        <div className="board-row">
-          {this.renderSquare(0)}
-          {this.renderSquare(1)}
-          {this.renderSquare(2)}
-        </div>
-        <div className="board-row">
-          {this.renderSquare(3)}
-          {this.renderSquare(4)}
-          {this.renderSquare(5)}
-        </div>
-        <div className="board-row">
-          {this.renderSquare(6)}
-          {this.renderSquare(7)}
-          {this.renderSquare(8)}
-        </div> */}
       </div>
     );
   }
